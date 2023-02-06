baaadf75d47714652cd757119068fd5cdc326d87
Yang Zhen <yz_rus1995@163.com>
2023-02-06 14:08:21 +0800

    third commit

--- MetaNN/facilities/traits.h
+++ MetaNN/facilities/traits.h
@@ -6,7 +6,7 @@ namespace MetaNN
     template <typename T>
     struct Identity_
     {
-        using type = T;
+        using type = T;q
     };
 
     template <bool cur, typename TNext>
@@ -15,6 +15,7 @@ namespace MetaNN
     template <typename TNext>
     constexpr static bool OrValue<false, TNext> = TNext::value;
 
-    
+    template <bool cur, typename TNext>
+    constexpr static bool AndValue = true;
 
 }
